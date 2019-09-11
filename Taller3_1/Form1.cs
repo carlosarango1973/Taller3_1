2dc4cbfc5c06490f6a7bb4bbacff6255a5f8d635
Carlos Arango Betancurt <carlos.arango.b.2019@gmail.com>
2019-09-11 18:34:18 -0500

    Realizar un programa que solicite la edad de una persona y verifique si la persona es mayor o no de edad

---
+++ Taller3_1/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Taller3_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Convert.ToInt16(txtedad.Text) > 18)
+                lblmensaje.Text = "Mayor de edad";
+            else
+                lblmensaje.Text = "menor de edad";
+        }
+    }
+}
